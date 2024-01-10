076f1ee8d4842e8d34266054b27d01f2d3e020f6
AyhamAluthman <ayhamzain4@gmail.com>
2024-01-10 15:42:47 +0300

    work on navbar

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -4,22 +4,22 @@ import S from '../images/pha.webp'
 const Nav=()=>{
 return(
     <>  
-    <div className="h-16 bg-gray-200">
+    <div className="h-16 bg-gray-200 fixed w-full top-0  ">
    
       <div className="  flex justify-between items-center  px-20       ">
            <div className=" py-1"><img src={S} alt="not found" className="h-14  rounded-3xl  "/></div>  
           <div > 
-              <ul >
+              <ul  className=" invisible md:visible ">
                 <li>
-                  <a href=""className="mr-10 font-mono  text-l hover:text-blue-800">home</a>
-                  <a href=""className="mr-10 font-mono  text-l hover:text-blue-800">servies</a>
-                  <a href=""className="mr-10 font-mono  text-l hover:text-blue-800">any</a>
-                  <a href=""className="mr-10 font-mono  text-l hover:text-blue-800">any</a>
+                  <a href=""className=" text-lg font-bold mr-10 font-mono  text-l hover:text-blue-800">Home</a>
+                  <a href=""className=" text-lg font-bold mr-10 font-mono  text-l hover:text-blue-800">Servies</a>
+                  <a href=""className=" text-lg font-bold mr-10 font-mono  text-l hover:text-blue-800">Pricing</a>
+                  <a href=""className=" text-lg font-bold mr-10 font-mono  text-l hover:text-blue-800">News</a>
                 </li>
                 
               
               </ul> </div>
-          <div className=" font-mono   bg-white   rounded-xl  h-10 w-20 px-4 py-1 text-blue-500 hover:bg-blue-700 "> <button className=" pt-1  ">Signup</button>  </div>
+          <div className=" font-mono   bg-white   rounded-xl  h-10 w-auto px-4 py-1 text-blue-500  hover:bg-blue-700  "> <button className="   text-xl leading-8    ">Signup</button>  </div>
       </div>
  
 
